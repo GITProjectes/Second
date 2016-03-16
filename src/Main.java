5831d354c618f5f3e71cea4fb185925b684dd171
MaxKuznetsov <maxalkuz28@mail.ru>
2016-03-16 16:21:43 +0200

    maximus

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 
 public class Main {
-	String alena = "17:08,17:09";
+	String alena = "17:08";
+	String max = "17:20";
 
 	public static void main(String[] args) {
 		new Main();
 	}
 
 	Main() {
-	System.out.println(alena);
-
+		System.out.println(alena);
+		System.out.println(max);
 	}
 }
